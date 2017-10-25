45b5f476f40209286a65ecf542d6258d5befca6e
Olaf Veerman <olaf.veerman@gmail.com>
2017-10-25 13:07:44 -0400

    Enable button to add points

--- modules/ui/modes.js
+++ modules/ui/modes.js
@@ -15,7 +15,7 @@ import { uiTooltipHtml } from './tooltipHtml';
 
 export function uiModes(context) {
     var modes = [
-        // modeAddPoint(context),
+        modeAddPoint(context),
         modeAddLine(context)
         // modeAddArea(context)
     ];
